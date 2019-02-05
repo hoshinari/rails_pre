bf4431c3f21d9fb0df813093d7d8a72815f6e6b2
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2019-02-06 00:56:55 +0900

    Finish chapter13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
   def index
-    
   end
 
   def show
-    @user = Hash.new
-    @user[:name] = 'Shohei Aoki'
-    @user[:username] = 'moyahima'
-    @user[:location] = 'Tottori, Japan'
-    @user[:about] = 'Nice to meet you.'
+      @user = User.find_by(:username => params[:username])
   end
 end
